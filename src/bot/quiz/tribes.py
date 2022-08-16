f5f87d78639aa6258ed3f6b1cd30f8a1f96ffbbf
DLCHAMP <36091350+dlchamp@users.noreply.github.com>
2022-08-16 11:11:00 -0500

    move tribe config to config.py

--- src/bot/quiz/tribes.py
+++ src/bot/quiz/tribes.py
@@ -1,27 +1,7 @@
 """Handle the Tribe classes"""
 from dataclasses import dataclass
 
-import disnake
-from bot.config import Config
-
-tribes = {
-    "Z": {
-        "role_id": Config.tribe_z_role_id,
-        "description": "Brave, astronomer/intellects, explorer, traveler, grounded, arrogant",
-    },
-    "P": {
-        "role_id": Config.tribe_p_role_id,
-        "description": "Leadership, care, decision making, hunter/resourceful, seamstress, kind, reliant",
-    },
-    "B": {
-        "role_id": Config.tribe_b_role_id,
-        "description": "Chef, farmer, jolly, confident, funny, weak, hard worker, loyal, fair",
-    },
-    "M": {
-        "role_id": Config.tribe_m_role_id,
-        "description": "Clever/witty, curious, observant, fishers, ambitious, adventurous, sneaky",
-    },
-}
+from disnake import Role
 
 
 @dataclass
@@ -30,4 +10,5 @@ class Tribe:
 
     name: str
     description: str
-    role: disnake.Role
+    icon_url: str
+    role: Role
